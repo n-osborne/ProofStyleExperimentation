4406a3562b79c4fad4332adbf55e5d26131ae30a
Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>
2019-08-28 14:28:58 +0200

    occurrences function and Inductive Prop

--- coq/InsertSort.v
+++ coq/InsertSort.v
@@ -21,4 +21,16 @@ Fixpoint insert_sort (l: list nat) : list nat :=
   | x :: xs => insert x (insert_sort xs)
   end.
 
+Fixpoint occurrences (n: nat) (l: list nat) : nat :=
+  match l with
+  | []     => O
+  | x :: xs => match eqb n x with
+             | true  => S (occurrences n xs)
+             | false => occurrences n xs
+             end
+  end.
 
+Inductive Occurrences (n: nat): list nat -> nat -> Prop :=
+| empty : Occurrences n [] O
+| same  : forall l o, Occurrences n l o -> Occurrences n (n :: l) (S o)
+| diff  : forall l o x, Occurrences n l o -> x <> n -> Occurrences n (x :: l) o.
