ae68f2335e81231cfaf7cc68cab20b66b8ce6bf3
Nicolas Osborne <nicolas.osborne.etu@univ-lille.fr>
2019-08-29 12:48:56 +0200

    [Proof] insert function preserves sorting

--- coq/InsertSort.v
+++ coq/InsertSort.v
@@ -1,7 +1,11 @@
 
 
 Require Import Nat.
+Require Import PeanoNat.
 Require Import Lists.List.
+Require Import Permutation.
+
+Require Import FunInd.
 
 Import ListNotations.
 
@@ -14,6 +18,8 @@ Fixpoint insert (n: nat) (l: list nat) : list nat :=
            | false => x :: (insert n xs)
            end
   end.
+
+Functional Scheme insert_ind := Induction for insert Sort Prop.
 
 Fixpoint insert_sort (l: list nat) : list nat :=
   match l with
@@ -39,3 +45,35 @@ Inductive Sorted: list nat -> Prop :=
 | nilSorted : Sorted []
 | singleSorted: forall x, Sorted [x]
 | consSorted : forall xs x n, Sorted (x :: xs) -> n <= x -> Sorted (n :: x :: xs).
+
+Lemma Sorted_xxs_Sorted_xs: forall x xs, Sorted (x :: xs) -> Sorted xs.
+Proof.
+  intros x xs ant.
+  inversion ant.
+  - apply nilSorted.
+  - assumption.
+Qed.
+
+Lemma Insert_Sorted: forall n l, Sorted l -> Sorted (insert n l).
+Proof.
+  intros n; induction l as [|x xs IH]; intro ant.
+  - now apply singleSorted.
+  - functional induction (insert n (x :: xs)).
+    + apply singleSorted.
+    + apply consSorted; trivial.
+      now apply Nat.leb_le.
+    + functional induction (insert n xs0).
+      * apply consSorted.
+        ** apply singleSorted.
+        ** apply Nat.leb_gt in e0; now apply Nat.lt_le_incl.
+      * apply consSorted.
+        ** inversion ant.
+           now apply IHl0 in H1.
+        ** apply Nat.leb_gt in e0; now apply Nat.lt_le_incl.
+      * apply consSorted.
+        ** apply Sorted_xxs_Sorted_xs in ant.
+           now apply IHl0 in ant.
+        ** now inversion ant.
+Qed.
+
+        
